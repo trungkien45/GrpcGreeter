695cf15609601ea56caaa945bff7215f623b8fff
kien <ntkien45@gmail.com>
2022-12-09 16:38:13 +0700

    ggg

--- GrpcGreeter/Program.cs
+++ GrpcGreeter/Program.cs
@@ -7,9 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
-
+app.MapGrpcReflectionService();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGrpcService<Greeter3Service>();
